97b7da31ae613681091b2dec9e71acc5efdd4e42
Vladislavichek <vlmulg@taltech.ee>
2025-09-25 14:34:27 +0300

    Changed the wheel collision box to proper 8 mm instead of 80 mm, added the collisions, mass and inerta

--- build/ias0220_250620/prefix_override/sitecustomize.py
+++ build/ias0220_250620/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/vlados/ros2_ws/install/ias0220_250620'
+    sys.prefix = sys.exec_prefix = '/home/vlados/ias0220/install/ias0220_250620'
